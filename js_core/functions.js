4e6ebb57fdbb9271bd154bbecebacb2128260b77
Daniil Yazvinski <daniil.yazvinskiy@gmail.com>
2024-06-24 16:27:51 +0300

    feat: functions

---
+++ js_core/functions.js
@@ -0,0 +1,28 @@
+const bubbleSort = (arr) => {
+    if(arr.length === 0){
+        return 'Массив пустой!'
+    }
+    for (let i = 0; i < arr.length; i++) {
+        let swapped = false;
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+    return arr;
+}
+
+let arr = [64, 34, 25, 12, 22, 11, 90];
+let sortedArr = bubbleSort(arr);
+console.log(sortedArr); // [11, 12, 22, 25, 34, 64, 90]
+
+let arr1 = [];
+let sortedArr1 = bubbleSort(arr1);
+console.log(sortedArr1); // [11, 12, 22, 25, 34, 64, 90]
